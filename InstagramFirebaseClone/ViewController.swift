56dec55c3ae22c62897599f7183e34915ca47e12
Mustafa <wadupbiatch@icloud.com>
2021-10-08 23:01:05 +0300

    Initial Commit

---
+++ InstagramFirebaseClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramFirebaseClone
+//
+//  Created by Mustafa on 8.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
